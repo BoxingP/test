c11736bde6fea8da96dfbe0f0d85da3fcb04d7e1
= <=>
2021-06-22 18:58:22 +0530

    Updated backend settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000'
+    'http://localhost:3000'
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
